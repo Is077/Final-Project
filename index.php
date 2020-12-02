c743c3ecb852b8c3b8c008f265274b21b03d6886
Israel Orevaoghene <israel.orevaoghene@ashesi.edu.gh>
2020-12-02 21:03:11 +0000

    updaate

--- index.php
+++ index.php
@@ -13,16 +13,17 @@
         <link rel="stylesheet" type="text/css" href="index.css">
         <link href='https://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet'>
     </head>
+
     <body>
         <div class="login-page">
-          <img src="complogo.png" alt="Company Logo" id="logo">
-          <p class="long-text"><b>Where Learning in Fun!</b></p>
+          <img src="logo/complogo.png" alt="Company Logo" id="logo">
+          <p class="long-text"><b><h2>Where Learning in Fun!</h2></b></p>
            <div class="form">
               <form id="login-form" class="login-form" method="POST" target="">
                 <div class="form-fill">
-                  <p style="color: blue; font-size:12px;"><?php if(isset($errorss['validate_mail'])) echo $errorss['validate_mail']; ?></p>
-                  <p style="color: blue; font-size:12px;"><?php if(isset($errorss['validate_pass'])) echo $errorss['validate_pass']; ?></p>
-                  <p style="color: blue; font-size:12px;"><?php if(isset($errorss['validate'])) echo $errorss['validate']; ?></p>
+                  <p style="color: red; font-size:12px;"><?php if(isset($errorss['validate_mail'])) echo $errorss['validate_mail']; ?></p>
+                  <p style="color: red; font-size:12px;"><?php if(isset($errorss['validate_pass'])) echo $errorss['validate_pass']; ?></p>
+                  <p style="color: red; font-size:12px;"><?php if(isset($errorss['validate'])) echo $errorss['validate']; ?></p>
                   <label for="Fullname">Email</label>
                   <input id="Email" name="Email" value="<?php echo $UserEmail['Email']; ?>" placeholder="Email" />
                   <small>Error Message</small>
